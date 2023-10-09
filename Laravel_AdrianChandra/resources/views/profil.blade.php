3a48d3a10d9c6d61e383c2a9f324dbb541374438
Drian73 <140342814+Drian73@users.noreply.github.com>
2023-10-09 13:12:10 +0700

    Route

---
+++ Laravel_AdrianChandra/resources/views/profil.blade.php
@@ -0,0 +1,1 @@
+<h1>Halo saya HHAHIHI</h1>
--- Laravel_AdrianChandra/routes/web.php
+++ Laravel_AdrianChandra/routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//BUAT ROUTE KE PROFIL
+Route::get("/profil", function (){
+    return view("profil");
+});
+
+//ROUTE DGN PARAMETER (WAJIB)
+Route::get("/mahasiswa/{nama}", function ($nama = "Adrian"){
+    echo "<h1>Halo nama saya $nama</h1>";
+});
+
+//ROUTE DGN PARAMETER (TDK WAJIB)
+Route::get("/mahasiswa2/{nama?}", function ($nama = "Adrian"){
+    echo "<h1>Halo nama saya $nama</h1>";
+});
+
+//ROUTE DGN PARAMETER > 1
+Route::get("/profil2/{nama?}/{pekerjaan?}", function ($nama = "Adrian", $pekerjaan = "Mahasiswa"){
+    echo "<h1>Halo nama saya $nama, saya adalah $pekerjaan</h1>";
+});
+
+Route::get("/hubungi", function (){
+    echo "<h1>Hubungi kami</h1>";
+})->name("call");
+
+Route::redirect("/contact", "/hubungi");
+
+Route::get("/halo", function (){
+    echo "<a href = '". route('call'). "'>". route('call'). "</a>";
+});
+
+Route::prefix("/dosen")->group(function () {
+    Route::get("/jadwal", function(){
+        echo "<h1> Jadwal Baru </h1>";
+    });
+    Route::get("/materi", function (){
+    echo "<h1>Materi Perkuliahan</h1>";
+    });
+});
