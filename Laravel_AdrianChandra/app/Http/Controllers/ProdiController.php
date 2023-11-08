0228de031917a1a52416dcf15c327c6436f2afe1
Drian73 <adrianchandra190205@gmail.com>
2023-11-08 13:12:22 +0700

    Form Validation

--- Laravel_AdrianChandra/app/Http/Controllers/ProdiController.php
+++ Laravel_AdrianChandra/app/Http/Controllers/ProdiController.php
@@ -33,4 +33,29 @@ class ProdiController extends Controller
             echo "<hr>";
         }
     }
+
+    public function create() {
+        return view('prodi.create');
+    }
+
+    public function store(Request $request) {
+        //dump ($request);
+        //echo $request->nama;
+
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+        //dump($validateData);
+        //echo $validateData['nama'];
+
+        $prodi = new Prodi(); //buat object prodi
+        $prodi->nama = $validateData['nama']; //simpan nilai input ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
+        $prodi->save(); //simpan ke dalam tabel prodis
+
+        //return "Data prodi $prodi->nama berhasil disimpan ke database"; //tampilkan pesan berhasil
+        session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+        return redirect()->route('prodi.create');
+
+    }
+
 }
